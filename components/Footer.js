ba594782bba17e17a239d5a4a31d69fc8531a989
Rahul Suresh <rahul@criodo.com>
2021-12-11 22:25:45 +0530

    added code

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,20 @@
-import styles from './Footer.module.css'
+import * as styles from "./Footer.module.scss";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        <Image
+          src="/netliheart.svg"
+          width={100}
+          height={400}
+          className="image"
+        ></Image>
+        Made with{" "}
+        <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} />{" "}
+        for you
       </footer>
     </>
-  )
+  );
 }
